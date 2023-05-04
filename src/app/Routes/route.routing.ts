3838be2a7671d9d7b57a83b38d32fcefa95d8010
lenovo <lenovo@DESKTOP-DAC9JJD>
2023-05-04 15:30:43 +0100

    created user sign up and login in pages

--- src/app/Routes/route.routing.ts
+++ src/app/Routes/route.routing.ts
@@ -2,8 +2,15 @@ import { Routes } from '@angular/router';
 import { NotfoundComponent } from '../notfound/notfound.component';
 import { IndexComponent } from '../index/index.component';
 
+import { LoginComponent } from '../components/login/login.component';
+import { SignupComponent } from '../components/signup/signup.component';
+import { DashboardComponent } from '../components/dashboard/dashboard.component';
 export const routes: Routes = [
-  { path: '', component: IndexComponent },
-  { path: 'pageNotFound', component: NotfoundComponent },
-  { path: '**', redirectTo: 'pageNotFound', pathMatch: 'full' },
-];
+  {path: '', component: IndexComponent },
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'dashboard',component:DashboardComponent},
+  {path: 'pageNotFound', component: NotfoundComponent },
+  { path: '**', redirectTo: 'pageNotFound', pathMatch: 'full' }
+    ];
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import{HttpClientModule} from "@angular/common/http";
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { AdminComponent } from './admin/admin.component';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { IndexComponent } from './index/index.component';
 import { RouteModule } from './Routes/route.module';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
@@ -13,8 +18,11 @@ import { RouteModule } from './Routes/route.module';
     AdminComponent,
     NotfoundComponent,
     IndexComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent
   ],
-  imports: [BrowserModule, RouteModule],
+  imports: [BrowserModule, RouteModule,ReactiveFormsModule,HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
